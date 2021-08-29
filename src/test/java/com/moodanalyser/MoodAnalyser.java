24ba7c5f21c55b1720004a8a97ac437121598fad
SRC0747 <RjSrc@0747>
2021-08-29 21:17:18 +0530

    Welcome to Mood Analyser Detectpr Problem.

---
+++ src/test/java/com/moodanalyser/MoodAnalyser.java
@@ -0,0 +1,8 @@
+package com.moodanalyser;
+
+public class MoodAnalyser {
+    public static void main(String[] args)
+    {
+        System.out.println("Mood Analyser to analyze Haappy or Sad Mood.");
+    }
+}
