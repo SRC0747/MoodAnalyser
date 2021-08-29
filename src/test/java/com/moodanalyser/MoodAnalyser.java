81e81c97813c1457e0564d6d0f1c2ee824f54a59
SRC0747 <RjSrc@0747>
2021-08-30 01:27:14 +0530

    added: Analyse Mood using Constructor.

--- src/test/java/com/moodanalyser/MoodAnalyser.java
+++ src/test/java/com/moodanalyser/MoodAnalyser.java
@@ -1,6 +1,10 @@
 package com.moodanalyser;
 
 public class MoodAnalyser {
+    String mood_swing ;
+    MoodAnalyser(String mood_swing){
+        this.mood_swing = mood_swing;
+    }
     public void analyseMood(String mood){
         System.out.println("Current Mood is:"+mood);
     }
@@ -9,5 +13,7 @@ public class MoodAnalyser {
         System.out.println("Mood Analyser to analyze Haappy or Sad Mood.");
         MoodAnalyser obj = new MoodAnalyser();
         obj.analyseMood("Happy");
+        MoodAnalyser obj_swing = new MoodAnalyser("Sad");
+        System.out.println("My Mood is:"+obj_swing);
     }
 }
