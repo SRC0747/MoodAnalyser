683249fa16c2045a8218cc6911b40088106386e6
SRC0747 <RjSrc@0747>
2021-08-29 22:50:19 +0530

    added: Analyse mood Happy or Sad.

--- src/test/java/com/moodanalyser/MoodAnalyser.java
+++ src/test/java/com/moodanalyser/MoodAnalyser.java
@@ -1,8 +1,13 @@
 package com.moodanalyser;
 
 public class MoodAnalyser {
+    public void analyseMood(String mood){
+        System.out.println("Current Mood is:"+mood);
+    }
     public static void main(String[] args)
     {
         System.out.println("Mood Analyser to analyze Haappy or Sad Mood.");
+        MoodAnalyser obj = new MoodAnalyser();
+        obj.analyseMood("Happy");
     }
 }
